53628a2f80f1e8b5cae1459016f1a6d1bebc141b
unknown <ramon.naef@students.unibe.ch>
2025-05-25 21:59:25 +0200

    Main

--- src/Main.py
+++ src/Main.py
@@ -5,7 +5,9 @@ from pymoo.problems import get_problem
 
 if __name__ == "__main__":
     dm = InteractiveDM()
-    problem = get_problem("ackley")
+
+    problem = get_problem("bnh")
+
 
     def f0(x):
         return 4 * x[0] * x[0] + 4 * x[1] * x[1]
@@ -17,8 +19,9 @@ if __name__ == "__main__":
 
         return term1 * term1 + term2 * term2
 
-    engine = IMO_DRSAEngine()
 
-    engine.fit(problem=problem)
+    objectives = [f0, f1]
 
-    engine.solve(dm)
+    engine = IMO_DRSAEngine().fit(problem=problem, objectives=objectives)
+
+    success = engine.solve(dm, True)
